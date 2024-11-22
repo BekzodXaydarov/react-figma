4a91230b15409a319bae02ab1ff292a7763bbfc9
Takitok2011 <bekzodeoke@gmail.com>
2024-11-22 20:48:22 +0300

    m

--- src/components/utils/index.ts
+++ src/components/utils/index.ts
@@ -6,62 +6,62 @@ export interface DestinationDataI {
 
 export const DestinationData: DestinationDataI[] = [
   {
-    img: "/public/images/destination1.png",
+    img: "/images/destination1.png",
     title: "Malaysia",
     hours: "16 Tours",
   },
   {
-    img: "/public/images/destination2.png",
+    img: "/images/destination2.png",
     title: "Dubai",
     hours: "16 Tours",
   },
   {
-    img: "/public/images/destination3.png",
+    img: "/images/destination3.png",
     title: "Greece",
     hours: "16 Tours",
   },
   {
-    img: "/public/images/destination4.png",
+    img: "/images/destination4.png",
     title: "Singapore",
     hours: "16 Tours",
   },
   {
-    img: "/public/images/destination1.png",
+    img: "/images/destination1.png",
     title: "Malaysia",
     hours: "16 Tours",
   },
   {
-    img: "/public/images/destination2.png",
+    img: "/images/destination2.png",
     title: "Dubai",
     hours: "16 Tours",
   },
   {
-    img: "/public/images/destination3.png",
+    img: "/images/destination3.png",
     title: "Greece",
     hours: "16 Tours",
   },
   {
-    img: "/public/images/destination4.png",
+    img: "/images/destination4.png",
     title: "Singapore",
     hours: "16 Tours",
   },
   {
-    img: "/public/images/destination1.png",
+    img: "/images/destination1.png",
     title: "Malaysia",
     hours: "16 Tours",
   },
   {
-    img: "/public/images/destination2.png",
+    img: "/images/destination2.png",
     title: "Dubai",
     hours: "16 Tours",
   },
   {
-    img: "/public/images/destination3.png",
+    img: "/images/destination3.png",
     title: "Greece",
     hours: "16 Tours",
   },
   {
-    img: "/public/images/destination4.png",
+    img: "/images/destination4.png",
     title: "Singapore",
     hours: "16 Tours",
   },
@@ -124,19 +124,19 @@ export interface TourDataI {
 
 export const TourData: TourDataI[] = [
   {
-    img: "/public/images/rome.png",
+    img: "/images/rome.png",
     title: "rome, italy",
     day: 10,
     price: "5.24K"
   },
   {
-    img: "/public/images/pakistan.png",
+    img: "/images/pakistan.png",
     title: "Lahore, pakistan",
     day: 6,
     price: "3.24K"
   },
   {
-    img: "/public/images/singapur.png",
+    img: "/images/singapur.png",
     title: "singapore",
     day: 6,
     price: "7.00K"
@@ -150,32 +150,32 @@ export interface PhotosI {
 }
 export const PhotosData: PhotosI[] = [
   {
-    img: '/public/images/photo1.png',
+    img: '/images/photo1.png',
     title: "title",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
   },
   {
-    img: '/public/images/photo2.png',
+    img: '/images/photo2.png',
     title: "title",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
   },
   {
-    img: '/public/images/photo3.png',
+    img: '/images/photo3.png',
     title: "title",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
   },
   {
-    img: '/public/images/photo4.png',
+    img: '/images/photo4.png',
     title: "title",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
   },
   {
-    img: '/public/images/photo5.png',
+    img: '/images/photo5.png',
     title: "title",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
   },
   {
-    img: '/public/images/photo6.png',
+    img: '/images/photo6.png',
     title: "title",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
   },
